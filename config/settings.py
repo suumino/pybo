da37d3160b4b7dd5b8a8c6d57087265442a59aee
suumino <anmin6@naver.com>
2021-03-23 22:53:38 +0900

    ALLOWD_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r!qn%w%xqpf60ar@t$_mvp#k#)3*sa^cj7el_bdmc)g3n)v-@)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.36.163.191']
 
 
 # Application definition
